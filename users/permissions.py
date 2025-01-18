855af8b5bc1bd2b87166e62e1de66bb6a17a575f
Павел Сергеевич <p.bitiakov.official@gmail.com>
2025-01-18 16:20:21 +0300

    Исправление сайта и отображения + добавление условий по роли пользователей

--- users/permissions.py
+++ users/permissions.py
@@ -5,6 +5,16 @@ class IsOwner(BasePermission):
     """Проверка, является ли пользователь владельцем"""
 
     def has_object_permission(self, request, view, obj):
-        if obj.user == request.user:
-            return True
-        return False
+        # Сначала проверяем, авторизован ли пользователь
+        if not request.user.is_authenticated:
+            return False
+        return obj.owner == request.user
+
+class IsModer(BasePermission):
+    """Проверяет, является ли пользователь модератором"""
+
+    def has_permission(self, request, view):
+        # Сначала проверяем, авторизован ли пользователь
+        if not request.user.is_authenticated:
+            return False
+        return request.user.groups.filter(name="moder").exists()
